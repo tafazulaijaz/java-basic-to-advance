681d0b095b069ddbcf9fd90e3b57571e61fc55e5
tafazulaijaz <tafazulqureshi1@gmail.com>
2026-01-08 17:49:56 +0530

    Create objects.js
    today i learned objects in javascript

---
+++ objects.js
@@ -0,0 +1,23 @@
+const myName = "tafazul"
+const user = {
+  firstName: "tafazul",
+  lastName: "aijaz",
+  age: 16,
+  education: 10,
+  location: "kashmir",
+};
+const user2 = {
+  firstName: "tahir",
+  lastName: "aijaz",
+  address : {
+    city :"srinagar",
+    pincode : 123001,
+    state :"kashmir"
+
+  }
+
+};
+
+console.log(user2);
+user2.relation = 'brother'
+user2["is-Student"] = true
