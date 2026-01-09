6f310622d75d6dbf90c57fdfef5213872dd5af99
tafazulaijaz <tafazulqureshi1@gmail.com>
2026-01-09 15:54:37 +0530

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,14 @@
+const numbers = ['5', '1', '3', '2', '4']
+const words = ['orange','banana', 'chocolate']
+const vowels = ['A','E','I','O','U']
+const newString = numbers.concat(words).concat(vowels)
+console.log(newString);
+const nameAndNumberList = [
+  ["tafazul", 100],
+  ["tahir", 85],
+];
+const ticTacToe = [
+  ["X", null, null],
+  [null, null, "O"],
+  ["O", null, "X"],
+];
