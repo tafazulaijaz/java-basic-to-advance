7e2d3506cfaa30d14b037b0e208b368cdc414d60
tafazulaijaz <tafazulqureshi1@gmail.com>
2026-01-13 17:04:55 +0530

    Create settimeout.js

---
+++ settimeout.js
@@ -0,0 +1,11 @@
+console.log('hi');
+
+function hello() {
+    console.log('hello world');
+
+}
+for(let i = 1; i <= 4; ++i) {
+    console.log(i);
+}
+setTimeout(`console.log('hello')`,2000)
+console.log('program ended');
